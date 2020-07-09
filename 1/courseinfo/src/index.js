f64995200e6a9819f55b6aa20335203fa40ff7dc
TraDukTer <jani.suutarinen@gmail.com>
2020-07-09 15:57:33 +0300

    exercises 1.3 - 1.5

--- 1/courseinfo/src/index.js
+++ 1/courseinfo/src/index.js
@@ -1,33 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      {/* <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
-
       <Header course = {course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Content course = {course} />
+      <Total course = {course} />
     </div>
   )
 }
@@ -35,7 +34,7 @@ const App = () => {
 const Header = (props) =>  {
   return (
     <h1>
-      {props.course}
+      {props.course.name}
     </h1>
   )
 }
@@ -44,11 +43,15 @@ const Content = (props) =>  {
   return (
     <div>
       { 
-      <>
-        <Part name = {props.part1} exercises = {props.exercises1} />
-        <Part name = {props.part2} exercises = {props.exercises2} />
-        <Part name = {props.part3} exercises = {props.exercises3} />
-      </>
+      <div>
+        {/* const parts = props.parts.map(value => <Part part = {value}/>)
+        
+        const parts = t.map(value => '<li>' + value + '</li>') */}
+        
+        <Part part = {props.course.parts[0]} />
+        <Part part = {props.course.parts[1]} />
+        <Part part = {props.course.parts[2]} />
+      </div>
       }
     </div>
   )
@@ -59,7 +62,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
       </p>
     </div>
   )
@@ -68,7 +71,7 @@ const Total = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.name} {props.exercises} 
+      {props.part.name} {props.part.exercises} 
     </p>
   )
 }
